e0368c0448d0880f306d36b170e3526c611f4f1a
isy6439 <130421508+isy6439@users.noreply.github.com>
2024-05-13 05:03:48 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -2,7 +2,7 @@ def solution(my_string):
     answer = ''
     vowel = ['a', 'e', 'i', 'o', 'u']
     answer = [s for s in my_string if s not in vowel]
-    # for v in vowel : 
-    #     my_string.replace(v, '')
+    for v in vowel : 
+        my_string = my_string.replace(v, '')
         
-    return ''.join(answer)
+    return my_string
